7c21efb47ec55035cab635691436046831199535
Unknown <mzolmajd@yahoo.com>
2017-01-04 14:17:38 -0500

    Added repos

---
+++ position-dashboard-service/src/main/java/com/ebc/rbcone/position/dashboard/repo/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.ebc.rbcone.position.dashboard.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<String, Long> {
+
+}
