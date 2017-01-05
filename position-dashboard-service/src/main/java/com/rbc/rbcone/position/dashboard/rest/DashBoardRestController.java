5c0403199bbef450e25779129a951c5fce630f28
John Mis <john.mis@rbc.com>
2017-01-05 08:13:00 -0500

    Fixups to back end.

--- position-dashboard-service/src/main/java/com/rbc/rbcone/position/dashboard/rest/DashBoardRestController.java
+++ position-dashboard-service/src/main/java/com/rbc/rbcone/position/dashboard/rest/DashBoardRestController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rbc.rbcone.position.dashboard.service.AccountService;
@@ -28,7 +28,7 @@ public class DashBoardRestController {
 	}
 
 	@GetMapping(path="accounts/{accountNumber}/holdings", produces=MediaType.APPLICATION_JSON_VALUE)
-	public AccountHoldingDTO getHoldings(@RequestParam(name="accountNumber", defaultValue="ALL", required=true) String accountNumber) {
+	public AccountHoldingDTO getHoldings(@PathVariable(name="accountNumber", required=true) String accountNumber) {
 		return accountService.getHoldings(accountNumber);
 	}
 
