970b1bc61523ed20e2463d6a1ac090dc5016a9a3
John Mis <john.mis@rbc.com>
2017-01-06 12:40:28 -0500

    Fixup errors in previous commit

--- mojo-map/src/app/news-feed/news-feed.component.ts
+++ mojo-map/src/app/news-feed/news-feed.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { $WebSocket } from 'angular2-websocket/angular2-websocket';
 
 @Component({
     selector: 'news-feed',
@@ -9,26 +8,11 @@ import { $WebSocket } from 'angular2-websocket/angular2-websocket';
 export class NewsFeedComponent implements OnInit, OnDestroy {
 
     news: string;
-    private websocket: $WebSocket;
 
     ngOnInit() {
         this.news = 'Some news';
-        this.websocket = new $WebSocket('ws://localhost:8080/newsfeed', null, {initialTimeout: 500, maxTimeout: 300000, reconnectIfNotNormalClose: true});
-        this.websocket.send('foo').subscribe(
-            (msg) => {
-                console.log("next", msg.data);
-            },
-            (msg) => {
-                console.log("error", msg);
-            },
-            () => {
-                console.log("complete");
-            }
-        );
-        this.websocket.send
     }
 
     ngOnDestroy() {
-        this.websocket.close(true);
     }
 }
