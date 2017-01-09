c4089e9745a2cb6c2a81c04bf5e85b60014494fc
John Mis <john.mis@rbc.com>
2017-01-09 10:45:53 -0500

    Add support for multiple websocket session.

---
+++ position-dashboard-service/src/main/java/com/rbc/rbcone/position/dashboard/news/NewsFeedSessionState.java
@@ -0,0 +1,46 @@
+package com.rbc.rbcone.position.dashboard.news;
+
+import java.util.Date;
+
+import org.springframework.web.socket.WebSocketSession;
+
+class NewsFeedSessionState {
+
+	private String currentTopic;
+	private String previousTopic;
+	private Date latestNewsDate;
+	private WebSocketSession session;
+
+	String getPreviousTopic() {
+		return previousTopic;
+	}
+
+	String getCurrentTopic() {
+		return currentTopic;
+	}
+
+	void setCurrentTopic(String currentTopic) {
+		if (this.currentTopic != null && !this.currentTopic.equals(currentTopic)) {
+			this.previousTopic = this.currentTopic;
+			this.currentTopic = currentTopic;
+			this.latestNewsDate = null;
+		}
+	}
+
+	Date getLatestNewsDate() {
+		return latestNewsDate;
+	}
+
+	void setLatestNewsDate(Date latestNewsDate) {
+		this.latestNewsDate = latestNewsDate;
+	}
+
+	WebSocketSession getSession() {
+		return session;
+	}
+
+	void setSession(WebSocketSession session) {
+		this.session = session;
+	}
+
+}
