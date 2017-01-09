b0c4724a8e3cdfcb7d00a79d3069053a58c126d5
John Mis <john.mis@rbc.com>
2017-01-09 10:07:38 -0500

    Put in a real search topic.

--- mojo-map/src/app/news-feed/news-feed.component.ts
+++ mojo-map/src/app/news-feed/news-feed.component.ts
@@ -41,7 +41,7 @@ export class NewsFeedComponent implements OnInit, OnDestroy {
     constructor(private newsFeedService: NewsFeedService) { }
 
     ngOnInit() {
-        this.topic = 'Some topic';
+        this.topic = 'IBM';
   		this.source = new LocalDataSource();
         this.newsFeedService
             .subscribeToNews(this.topic)
